14572f3b3824230afacf62bb86e72dd73b30471e
Justin Grimes <justinlgrimes@gmail.com>
2015-05-20 21:40:13 -0400

    Added a Login record and a LoginPailStructure that implements PailStructure.

--- src/big_data/core.clj
+++ src/big_data/core.clj
@@ -1,7 +1,39 @@
 (ns big_data.core
-  (:import [com.backtype.hadoop.pail Pail]))
+  (:import [com.backtype.hadoop.pail
+            Pail
+            PailStructure]
+           [java.io
+            ByteArrayOutputStream ByteArrayInputStream
+            DataOutputStream DataInputStream
+            IOException]
+           [java.lang RuntimeException]))
 
 
+(defrecord Login [user-name #^long login-unix-time])
+
+(defrecord LoginPailStructure []
+  PailStructure
+  (serialize [this login]
+    (let [byte-out (ByteArrayOutputStream.)
+          data-out (DataOutputStream. byte-out)
+          user-bytes (.getBytes (.user-name login))]
+      (try
+        (doto data-out
+          (.writeInt (alength user-bytes))
+          (.write user-bytes)
+          (.writeLong (.login-unix-time login))
+          (.close))
+        (catch IOException e
+          (throw (RuntimeException. e))))
+      (.toByteArray byte-out)))
+  (deserialize [this serialized]
+    (let [data-in (DataInputStream. (ByteArrayInputStream. serialized))]
+      (try
+        (let [user-bytes (byte-array (.readInt data-in))]
+          (.read data-in user-bytes)
+          (Login. (String. user-bytes) (.readLong data-in)))
+        (catch IOException e
+          (throw (RuntimeException. e)))))))
 
 (defn simple-pail-example []
   (let [pail (Pail/create "/tmp/mypail")
